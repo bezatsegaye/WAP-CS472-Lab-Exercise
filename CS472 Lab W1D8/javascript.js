5410bc9747e79d2817ad09ac2ecde46acf0feab5
bezatsegaye <62575823+bezatsegaye@users.noreply.github.com>
2020-03-31 17:36:30 -0500

    Add files via upload

--- CS472 Lab W1D8/javascript.js
+++ CS472 Lab W1D8/javascript.js
@@ -70,15 +70,64 @@ employee.setName("Tsegaye");
 employee.setAge(45);
 employee.setSalary(10,000);
 
-console.log(employee.getName());
-console.log(employee.getAge());
-console.log(employee.getSalary());
+console.log("Name: "+employee.getName());
+console.log("Age: "+employee.getAge());
+console.log("Salary: " +employee.getSalary());
 
 employee.setName("Farouk");
 employee.setAge(67);
 employee.setSalary(50,000);
 
-console.log(employee.getName());
-console.log(employee.getAge());
-console.log(employee.getSalary());
+console.log("Name: "+employee.getName());
+console.log("Age: "+employee.getAge());
+console.log("Salary: " +employee.getSalary());
 
+
+    console.log("The result of assigment eleven after address is inserted daynamically: \n");
+    {
+let employee = (function(){
+    let name="";
+    let age =0;
+    let salary=0.0;
+    return {setName: function (newName){
+        name =newName;
+    },
+    setAge: function (newAge){
+        age =newAge;
+    },
+    setSalary: function (newSalary){
+        salary =newSalary;
+    },
+    getName: function (newName){
+        return name;
+    },
+    getAge:function getAge(newAge){
+        return age;
+    },
+    getSalary:function (newSalary){
+        return salary;
+    }
+}
+
+})()
+
+employee.setName("Farouk");
+employee.setAge(67);
+employee.setSalary(50,000);
+
+console.log("Name: "+employee.getName());
+console.log("Age: "+employee.getAge());
+console.log("Salary: " +employee.getSalary());
+
+employee.address = "";
+employee.setAddress = function(newAddress){
+employee.address =newAddress;
+}
+
+employee.getAddress = function(newAddress){
+return employee.address;
+}
+
+employee.setAddress("Merkato");
+console.log("Address: "+ employee.getAddress());
+}
